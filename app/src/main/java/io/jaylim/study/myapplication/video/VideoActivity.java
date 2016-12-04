801618966da7d7af7f6bf6e06733229c4e118750
Jay Lim <iejisung@gmail.com>
2016-12-04 19:40:49 +0900

    initialized video camera module

--- app/src/main/java/io/jaylim/study/myapplication/video/VideoActivity.java
+++ app/src/main/java/io/jaylim/study/myapplication/video/VideoActivity.java
@@ -2,18 +2,32 @@ package io.jaylim.study.myapplication.video;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import io.jaylim.study.myapplication.SingleFragmentActivity;
 
 /**
  * Created by jaylim on 11/25/2016.
  */
 
-public class VideoActivity extends AppCompatActivity {
+public class VideoActivity extends SingleFragmentActivity{
+
+  @Override
+  public Fragment createDefaultFragment() {
+    return VideoFragment.newInstance();
+  }
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
+    // Set fullscreen preview window
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    getWindow().setFlags(
+        WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        WindowManager.LayoutParams.FLAG_FULLSCREEN
+    );
     super.onCreate(savedInstanceState);
-
-
   }
 }
--- app/src/main/java/io/jaylim/study/myapplication/video/VideoFragment.java
+++ app/src/main/java/io/jaylim/study/myapplication/video/VideoFragment.java
@@ -1,8 +1,16 @@
 package io.jaylim.study.myapplication.video;
 
+import android.support.v4.app.Fragment;
+
 /**
  * Created by jaylim on 11/25/2016.
  */
 
-public class VideoFragment {
+public class VideoFragment extends Fragment{
+
+  public static VideoFragment newInstance() {
+    return new VideoFragment();
+  }
+
+
 }
